9ca2669a8199b9deca72b2b706546198b177f891
Martijn Wehrens <wehrens@amolf.nl>
2011-05-04 14:15:59 +0200

    working on samples directory (bd_test)

--- samples/bd_test/irr.py
+++ samples/bd_test/irr.py
@@ -2,6 +2,8 @@
 
 from bd import *
 import sys
+import gfrdbase
+import model
 
 def run(outfilename, T, N):
 
@@ -19,21 +21,24 @@ def run(outfilename, T, N):
 
 def singlerun(T):
 
-    w = World(1e-3, 3)
-    s = BDSimulator(w)
-
     sigma = 5e-9
     r0 = sigma
     D = 1e-12
     kf = 10 * sigma * D
 
-    m = ParticleModel()
+    m = model.ParticleModel(world_size=1e-5)
 
-    A = m.new_species_type('A', D/2, sigma/2)
-    B = m.new_species_type('B', D/2, sigma/2)
-    C = m.new_species_type('C', D/2, sigma/2)
+    A = model.Species('A', D/2, sigma/2)
+    B = model.Species('B', D/2, sigma/2)
+    C = model.Species('C', D/2, sigma/2)
+    # Couple these species to the previously defined particle model
+    m.add_species_type(A) # "m.add_species_type(species=A)" not
+                          # allowed due boost package
+    m.add_species_type(B) 
+    m.add_species_type(C) 
     
-    r1 = create_binding_reaction_rule(A, B, C, kf)
+    r1 = model.create_binding_reaction_rule(A, B, C, kf)
+
     m.network_rules.add_reaction_rule(r1)
 
     s.set_model(m)
@@ -42,6 +47,14 @@ def singlerun(T):
     particleB = s.place_particle(B, [(float(A['radius']) + float(B['radius']))+1e-23,0,0])
 
     end_time = T
+
+    # Create world size m, with matrix_size sized
+    # neighbour matrix
+#    w = gfrdbase.create_world(m=1e-3, matrix_size=3)
+    w = gfrdbase.create_world(1e-3, 3)
+    s = BDSimulator(w)
+
+
     #s.initialize()
 
     while 1:
