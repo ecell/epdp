30c3566fad13257f5c3706df35c9cfba9b726fc0
Martijn Wehrens <wehrens@amolf.nl>
2011-05-16 16:50:31 +0200

    Mainly documentation update.
    While working on ../rebind/run.py ran into some functions I
    overlooked in gfrdbase.py.
    
    Thus added some functions to /doc/interface_functions.tex.
    
    Also, some convenient functions didn't have a docstring yet. Also
    added those. I've marked all docstrings I've added with:
    
    \#TODO: Added by wehrens@amolf.nl; please revise
    
    So, as the comment already said, please revise these docstrings.
    
    More info on /rebind will follow in next commit.
    
    ~MW

--- samples/rebind/run.py
+++ samples/rebind/run.py
@@ -18,75 +18,87 @@ import gfrdbase
 import model
 
 def run(outfilename, D_factor, N_B, N_X, N):
-    print outfilename
+    """ 
+    Function that loops over single runs of binding/rebinding
+    simulations. (Plus some overhead code.)
 
+    Arguments:
+        - 
+
+    """ #TODO
+    global DX_factor, V, L, matrix_size   
+
+    print "Printing output to: " + outfilename
+
+    ################# Set some constants 
     radius = 2.5e-9
     sigma = radius * 2
     D = 1e-12
     D_tot = D * 2
 
     tau = sigma**2 / D_tot
-    print 'tau=', tau
 
-    T_list = [tau * .1, INF]
-    # T_list = [INF] # With this option, the simulation stops immediately
-
-    outfile_t = open(outfilename + '_t.dat', 'w')
-    #outfile_r_list = [open(outfilename + '_r_-1.dat', 'w')] 
-
-    for i in range(N):
-        r_list, t_list = singlerun(T_list, D_factor, N_B, N_X)
-
-        for t in t_list:
-            outfile_t.write('%g\n' % t)
-
-        #for j in range(len(r_list)):
-        #    outfile_r_list[j].write('%g\n' % r_list[j])
-
-        print i, r_list, t_list
-        outfile_t.flush()
-        #[outfile_r.flush() for outfile_r in outfile_r_list]
-
-
-    outfile_t.close()
-    #[outfile_r.close() for outfile_r in outfile_r_list]
-
-
-
-def singlerun(T_list, D_factor, N_B, N_X):
-
-    # 100 nM = 100e-9 * N_A * 100 / m^3 = 6.02e19
-    # V = 1 / 6.02e19 = 1.66e-20 m^3
-    # L = 2.55e-7 m
-
-    # 1 uM = 6.02e20 / m^3
-    # V = 1.66e-21 m^3
-    # L = 1.18e-7
-
-    # ### Define constants
     DX_factor = 1
 
     V = 1e-18 # m^3
     L = V ** (1.0/3.0) 
 
-    matrix_size = min(max(3, int((9 * (N_X+N_B)) ** (1.0/3.0))), 60)
-    print 'matrix_size=', matrix_size
-
-    radius = 2.5e-9
-    sigma = radius * 2
-    r0 = sigma
-    D = 1e-12 * D_factor
-    D_tot = D * 2
-
-    tau = sigma**2 / D_tot
-
-    #kf = 1000 * sigma * D_tot
-    # 1e9 [1 / (M s)] -> 1e9 / 1000 / N_A [m^3 / s]
     kf = 0.092e-18
 
     DX = D * DX_factor
 
-    # ### Define model
+    matrix_size = min(max(3, int((9 * (N_X+N_B)) ** (1.0/3.0))), 60)
+    print 'matrix_size=', matrix_size
+
+    # This is the list with steps 
+    T_list = [tau * .1, INF]
+
+    # Clear the distance output file
+    outfile_r = open(outfilename + '_r_-'+j+'.dat', 'a')
+    outfile_r.close()
+
+    # Open the time output file
+    outfile_t = open(outfilename + '_t.dat', 'w')
+
+    ################# Loop through the multiple (N) simulations runs
+    for i in range(N):
+
+        # Perform a single simulation run
+        r_list, t_list = singlerun(T_list, D_factor, N_B, N_X)
+
+        # Write away time data
+        for t in t_list:
+            outfile_t.write('%g\n' % t)
+    
+        # Write away distance data, write data of different 
+        # measurement points to different files. (Ending up
+        # with a list of distances corresponding to measure-
+        # ments at times from T_list.)
+        for j in range(len(r_list)):
+            outfile_r = open(outfilename + '_r_-'+j+'.dat', 'a')
+            outfile_r.write('%g\n' % r_list[j])
+            outfile_r.close()
+
+        # Print to screen.
+        print i, r_list, t_list
+
+        # Make sure script writes data NOW
+        outfile_t.flush()
+
+    # Close time output file
+    outfile_t.close()
+
+def singlerun(T_list, D_factor, N_B, N_X):
+    """ 
+    Function that performs one simulation run of binding/
+    rebinding. 
+
+    Arguments:
+        -
+
+    """ #TODO
+
+    ################# Define the model:
     
     # Create model class
     m = model.ParticleModel(L)
@@ -108,13 +120,13 @@ def singlerun(T_list, D_factor, N_B, N_X):
     r2 = model.create_unbinding_reaction_rule(C, A, B, 1e3)
     m.network_rules.add_reaction_rule(r2)
 
-    # ### Set up simulator
+    ################# Set up simulator
 
     w = gfrdbase.create_world(m, matrix_size)
     nrw = gfrdbase.create_network_rules_wrapper(m)
     s = EGFRDSimulator(w, myrandom.rng, nrw)
 
-    # ### Place particles 
+    ################# Place particles 
 
     # Throw in some X particles and stirr
     if N_X != 0:
@@ -128,14 +140,10 @@ def singlerun(T_list, D_factor, N_B, N_X):
                 s.stop(end_time)
                 break
 
-
+    # Reset simulation, so stirring has no effect on output data
     s.reset()
-    # Removed introduction of network rules here.
-    # s.set_model(m) # IF presence of network rules results in data
-                     # that is not cleaned in reset, this might give 
-                     # problems. Originally, s.set_model(m) would 
-                     # have introduced these rules here.
 
+    # Define positions for particles to place.
     A_pos = [0,0,0]
     B_pos = [(float(A['radius']) + float(B['radius']))+1e-23,0,0]
 
@@ -235,12 +243,15 @@ def singlerun(T_list, D_factor, N_B, N_X):
                 print 'reaction: ', s.t - t_last
                 t_list.append(s.t - t_last)
 
+        """ If it's time to log, then log distance between particles. """
         next_time = s.get_next_time()
         if next_time > next_stop:
             print 'stop', i_T, next_stop
             s.stop(next_stop)
             if len(s.world.get_particle_ids(C.id)) != 0:  #A,B
                 r_list.append(0)
+            # If particles are manifested as A and B, log distance
+            # inbetween.
             else: # C = 0, i.e. there are particles A and B
                 # r_list.append(s.distance_between_particles(A.id, B.id))
                 # If there's only 1 B-particle
@@ -248,11 +259,16 @@ def singlerun(T_list, D_factor, N_B, N_X):
                 particlesB = w.get_particle_ids(B.id)
                 positionA = particlesA[1].position()
                 positionB = particlesB[1].position()
+
+                particle = self.sim.world.get_particle(particle_id)[1]
+
                 r_list.append(w.distance(positionA, positionB))
 
             i_T += 1
             next_stop = T_list[i_T]
         
+        """ If there are no measuring moments on the list any more,
+        stop """
         if next_stop == INF and len(t_list) != 0:
             print 'break', s.t
             break
@@ -270,3 +286,30 @@ if __name__ == '__main__':
         '_' #+ os.environ['SGE_TASK_ID']
     run(outfilename, float(sys.argv[1]), 
         int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]))
+
+
+
+
+"""
+
+Some additional notes:
+
+    # 100 nM = 100e-9 * N_A * 100 / m^3 = 6.02e19
+    # V = 1 / 6.02e19 = 1.66e-20 m^3
+    # L = 2.55e-7 m
+
+    # 1 uM = 6.02e20 / m^3
+    # V = 1.66e-21 m^3
+    # L = 1.18e-7
+
+"""
+
+
+
+
+
+
+
+
+
+
