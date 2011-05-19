98274611ef16e4042dfbc631f8d8c2f607a6d1af
Martijn Wehrens <wehrens@amolf.nl>
2011-05-19 12:00:57 +0200

    Added file from which one can easily copy and paste some basic code to get his/her script up and running more easily.

---
+++ samples/template/template.py
@@ -0,0 +1,100 @@
+
+# Template file to copy and paste basic code from.
+
+# Modules
+# ===============================
+import sys
+#Also, set relative python path
+sys.path.append('../../')
+import os
+
+from egfrd import *
+from bd import *
+import model
+import gfrdbase
+import _gfrd
+#import dumper
+
+
+# Constants
+# ===============================
+sigma = 1e-4        # Diameter particle; more realistic would be e-9
+D = 1e-12           # Diffusion constant
+N = 1000            # Number of steps simulation will last
+world_size = 1e-3   # Lengths of simulation box
+
+
+# Basic set up simulator
+# ===============================
+# Model
+m = model.ParticleModel(world_size)
+# Species
+X = model.Species('X', D, sigma/2)                                   
+m.add_species_type(A) 
+# Reaction rules
+r1 = model.create_binding_reaction_rule(A, B, X, k)
+m.network_rules.add_reaction_rule(r1)
+
+# World
+w = gfrdbase.create_world(m, 3)
+# Simulator
+s = EGFRDSimulator(w, myrandom.rng)
+
+# Throw in particles
+throw_in_particles(w, A, numberToThrowIn)
+
+
+# Running 1
+# ===============================
+while (s.get_next_time() < end_time)
+    s.step()
+
+s.stop(s.end_time)
+    
+
+# Running 2
+# ===============================
+for t in range(N):
+    s.step()
+
+s.stop(s.t)
+
+
+# Additional code
+# ===============================
+
+# VTK Logger
+# ===============================
+vtk_output_directory = 'VTK_out'
+if (os.path.exists(vtk_output_directory)):
+    print '** WARNING: VTK output directory already exists, possible '+ \
+            'present old VTK files might lead to errors.'
+    
+from visualization import vtklogger
+l = vtklogger.VTKLogger(s, vtk_output_directory, extra_particle_step=True) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
