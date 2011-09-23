e0818c3aa77d035cca4c33ba9ee870fdb5e2f0d9
Joris Paijmans <j.paijmans@amolf.nl>
2011-09-23 18:46:15 +0200

    Started on the new BD algorithm.

--- multi.py
+++ multi.py
@@ -18,7 +18,7 @@ class Multi(Domain):
 
         self.main = ref(main)
         self.last_event = None
-	self.event_Type = EventType.MULTI_DIFFUSION
+        self.event_Type = EventType.MULTI_DIFFUSION
 
         self.sphere_container = _gfrd.SphericalShellContainer(main.world.world_size, 3)
         self.particle_container = _gfrd.MultiParticleContainer(main.world)
