05a50eb2a2870ba676f08f92060cb9a1c1599802
Martijn Wehrens <wehrens@bionet3.amolf.nl>
2012-02-14 15:21:42 +0100

    Commented out some Gillespie code.

--- dumper.py
+++ dumper.py
@@ -2,7 +2,7 @@ from single import Single
 from pair import Pair
 from multi import Multi
 from egfrd import EGFRDSimulator
-from gillespie import GillespieSimulator
+#from gillespie import GillespieSimulator
 import _gfrd
 
 # get methods return an iterator, dump methods return a string.
@@ -116,9 +116,10 @@ def get_particles(sim, identifier=None):
             all (particle identifier, particle)-pairs will be returned.
 
     """
-    if isinstance(sim, GillespieSimulator):
-        raise RuntimeError('GillespieSimulator does not keep track '
-                           'of individual particles.')
+# TODO: Gillespie simulator should be legacy; remove
+#    if isinstance(sim, GillespieSimulator):
+#        raise RuntimeError('GillespieSimulator does not keep track '
+#                           'of individual particles.')
 
     if identifier == None:
         return sim.world
