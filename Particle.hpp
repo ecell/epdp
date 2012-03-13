d37f75cb66b0ef724277bc17f2583ee7b7f897f5
Laurens Bossen <bossen@amolf.nl>
2012-03-13 14:29:08 +0100

    removed old constructors

--- Particle.hpp
+++ Particle.hpp
@@ -31,14 +31,14 @@ struct Particle
     // Note that the structure_id is mandatory (?)
     Particle(): shape_(), species_id_(), structure_id_(), D_(0.), v_(0.) {}
 
-    Particle(species_id_type const& species_id, shape_type const& shape,
+/*    Particle(species_id_type const& species_id, shape_type const& shape,
              D_type const& D)
         : shape_(shape), species_id_(species_id), structure_id_(), D_(D), v_(0.){}
 
     Particle(species_id_type const& species_id, shape_type const& shape,
              D_type const& D, v_type const& v)
         : shape_(shape), species_id_(species_id), structure_id_(), D_(D), v_(v) {}
-
+*/
     Particle(species_id_type const& species_id, shape_type const& shape,
              structure_id_type const& structure_id, D_type const& D)
         : shape_(shape), species_id_(species_id), structure_id_(structure_id), D_(D), v_(0.){}
