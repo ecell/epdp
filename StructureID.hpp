8de5b7a4e93ca8f05c57438f2cf2141420502f8d
Kazunari Kaizu <kaizu@tcs2.gsc.riken.jp>
2012-04-27 19:53:01 +0900

    added StructureID class.

---
+++ StructureID.hpp
@@ -0,0 +1,59 @@
+#ifndef STRUCTURE_ID_HPP
+#define STRUCTURE_ID_HPP
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <ostream>
+#if defined(HAVE_TR1_FUNCTIONAL)
+#include <tr1/functional>
+#elif defined(HAVE_STD_HASH)
+#include <functional>
+#elif defined(HAVE_BOOST_FUNCTIONAL_HASH_HPP)
+#include <boost/functional/hash.hpp>
+#endif
+#include "Identifier.hpp"
+
+struct StructureID: public Identifier<StructureID, unsigned long long, int>
+{
+    typedef Identifier<StructureID, unsigned long long, int> base_type;
+
+    StructureID(value_type const& value = value_type(0, 0))
+        : base_type(value) {}
+};
+
+#if defined(HAVE_TR1_FUNCTIONAL)
+namespace std { namespace tr1 {
+#elif defined(HAVE_STD_HASH)
+namespace std {
+#elif defined(HAVE_BOOST_FUNCTIONAL_HASH_HPP)
+namespace boost {
+#endif
+
+template<>
+struct hash<StructureID>
+{
+    std::size_t operator()(StructureID const& val) const
+    {
+        return static_cast<std::size_t>(val().first ^ val().second);
+    }
+};
+
+#if defined(HAVE_TR1_FUNCTIONAL)
+} } // namespace std::tr1
+#elif defined(HAVE_STD_HASH)
+} // namespace std
+#elif defined(HAVE_BOOST_FUNCTIONAL_HASH_HPP)
+} // namespace boost
+#endif
+
+template<typename Tstrm_, typename Ttraits_>
+inline std::basic_ostream<Tstrm_, Ttraits_>& operator<<(std::basic_ostream<Tstrm_, Ttraits_>& strm,
+        const StructureID& v)
+{
+    strm << "PID(" << v().first << ":" << v().second << ")";
+    return strm;
+}
+
+#endif /* STRUCTURE_ID_HPP */
