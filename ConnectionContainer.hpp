1e3677ff030d9148a9f2cc48d4fbbecc33eab5f1
Laurens Bossen <bossen@amolf.nl>
2012-05-02 18:02:09 +0200

    mostly implemented the apply_boundary function for planes

--- ConnectionContainer.hpp
+++ ConnectionContainer.hpp
@@ -1,5 +1,3 @@
-
-
 template
 // overloaded typedef so as to fix the length of the vectors for different shape types
 // -Cylinder n=2
@@ -8,26 +6,63 @@ template
 
 
 
-template<typename StructureType_>
+template<typename Ttraits, Tobj_, std::size_t max_neighbors>
 class ConnectivityContainer
 // implements the way structures are connected.
-// It allows to to check what structure you enter into when you leave the current one.
+// It allows you to check what structure you enter into when you leave the current one.
 {
-    typedef typename StructureType_ structure_type;
+public:
+    typedef typename Ttraits        traits_type;
+    typedef typename Tobj_          obj_type;
+    typedef typename std::size_t    size_type;
+    typedef typename traits_type::structure_id_type structure_id_type;
+    typedef typename traits_type::position_type     position_type;
+
+protected:
+    // the structureID of the neighbor and the normal vector pointing from the
+    // intersection in the direction of the center of the neighbor
+    typedef std::pair<structure_id_type, position_type>                     structure_id_vector_pair_type;
+    typedef boost::array<structure_id_vector_pair_type, max_neighbors>      structure_id_vector_array_type;
+    // the mapping from the structure and all its neighbors and how they are connected.
+    typedef std::map<structure_id_type, structure_id_vector_array_type>     structid_neighbor_pair_map;
+    typedef select_second<typename structid_neighbor_pair_map::value_type>  structid_neighbor_second_selector_type;
+    typedef boost::transform_iterator<structid_neighbor_second_selector_type,
+            typename structid_neighbor_pair_map::const_iterator>            structid_neighbor_iterator;
 
 public:
-    position_structid_pair_type apply_boundary (position_structid_pair_type const& pos_structure_id) const
+    static const size_type max = max_neighbors;
+
+public:
+    virtual bool add_neighbor_info(obj_type const& obj) = 0;
+
+    virtual structure_id_vector_pair_type get_neighbor_info(structure_id_type const& id, size_type const n) const
     {
-        ::to_internal(structure_type::base_type.shape(), pos);
+        // check that the index is not too large
+        if ( n>= max )
+            throw not_found(std::string("Index out of range for neighbor (n= ") + boost::lexical_cast<std::string>(n) + ")");
+
+        typename structid_neighbor_pair_map::const_iterator i(structid_neighbor_pair_map_.find(id));
+        if (structid_neighbor_pair_map_.end() == i)
+        {
+            throw not_found(std::string("Neighbor structure not found(id=") + boost::lexical_cast<std::string>(id) + ")");
+        }
+        return (*i).second[n];
     }
 
-private:
-    // mapping StructureID -> vector<(StructureID, index), max_index+1>
+    virtual ConnectivityContainer()
+    // do we start with an empty container or do we construct it as a static object with n structures already connected.
 
+private:
+    // mapping StructureID -> array<(StructureID, vector3D), max_neighbors>
+    structid_neighbor_pair_map  neighbor_mapping_;
 };
 
 
 
+
+
+
+template <typename Ttraits>
 class StructureContainer
 {
 
@@ -43,15 +78,6 @@ public:
     add_structure (CuboidalRegion)
     {
         // add to Connectivity container for planar surfaces
-    }
-
-    position_structid_pair_type apply_boundary(position_structid_pair_type const& pos_structure_id) const
-    // Propagate these to the ParticleContainer such that world.apply_boundary( (pos, struct_id)) can be called
-    {
-    }
-    position_structid_pair_type cyclic_transpose(position_structid_pair_type const& pos_structure_id) const
-    // This is essentially the deflect_back function
-    {
     }
 
     remove_structure (StructureID)
@@ -149,3 +175,115 @@ protected:
     CuboidalReginConnectivityContainer          // This contains a StructureID -> vector <(StructureID, index), 6> map
 
 };
+
+
+
+//////// Inline functions applicable to the StructureContainer
+
+
+//// functions for Planes
+
+template<typename ConnectivityContainer<> >
+inline typename ConnectivityContainer::traits_type::position_structid_pair_type
+apply_boundary (StructureContainer const& sc, position_structid_pair_type const& pos_structure_id) const
+// The template needs to be parameterized with the appropriate shape (which then parameterizes the type
+// of the ConnectivityContainer that we use.
+// We supply the structure container as an argument so that we can get the structures that we need.
+// The connectivity container then needs to a part of the structure container such that we can also
+// query the boundary conditions and connectivity.
+{
+    // useful typedefs
+    typedef typename plane_type;
+    typedef typename plane_type::length_type    length_type;
+    typedef typename position_type;
+    typedef typename structure_id_type;
+    typedef typename position_structid_pair_type;
+
+    // Note that we assume that the new position is in the plane (dot(pos, unit_z)==0)
+    const plane_type origin_plane (sc.get_structure(pos_structure_id.second)->shape());
+    boost::array<length_type, 2> half_extends(origin_plane.half_extent());
+    const length_type origin_length_x (2*half_extends[0]);
+    const length_type origin_length_y (2*half_extends[1]);
+
+    // TODO do cyclic transpose here first
+    const position_type pos_vector( subtract(pos_structure_id.first, origin_plane.position()) );
+    const length_type component_x ( dot_product(pos_vector, origin_plane.unit_x()) );
+    const length_type component_y ( dot_product(pos_vector, origin_plane.unit_y()) );
+
+    // declare the variables that will be written
+    structure_id_type new_id;
+    position_type neighbor_plane_par;
+    position_type neighbor_plane_inl;
+
+    if ( (-half_extends[0] < component_x) && ( component_x < half_extends[0]) &&
+         (-half_extends[1] < component_y) && ( component_y < half_extends[1]) )
+    {
+        // we are still in the plane (did not pass any of the boundaries)
+        // don't have to do anything
+        return pos_structure_id;
+    }
+    else if ( (-half_extends[0] < component_x) && ( component_x < half_extends[0]) &&
+              (half_extends[1] < component_y) )
+    {
+        // we are at the 'top' of the plane (side nr. 0)
+        // get the unit vector pointing from the edge between the two planes to the center of the plane
+        const position_type neighbor_plane_unit_inline (sc.get_neighbor_info(pos_structure_id.second, 0).second);
+
+        new_id = neighbor_mapping_[pos_structure_id.second][0].first;
+        neighbor_plane_par = component_x * origin_plane.unit_x();
+        neighbor_plane_inl = (component_y - origin_length_y)* neighbor_plane_unit_inline;
+    }
+    else if ( (-half_extends[0] < component_x) && ( component_x < half_extends[0]) &&
+              (component_y < -half_extends[1] ) )
+    {
+        // we are at the 'bottom' of the plane (side nr. 1)
+        // get the unit vector pointing from the edge between the two planes to the center of the plane
+        const position_type neighbor_plane_unit_inline (sc.get_neighbor_info(pos_structure_id.second, 1).second);
+
+        new_id = neighbor_mapping_[pos_structure_id.second][1].first;
+        neighbor_plane_par = component_x * origin_plane.unit_x();
+        neighbor_plane_inl = (-component_y - origin_length_y)* neighbor_plane_unit_inline;
+    }
+    else if ( (-half_extends[1] < component_y) && ( component_y < half_extends[1]) &&
+              (component_x < -half_extends[0]) )
+    {
+        // we are at the 'left' of the plane (side nr. 2)
+        // get the unit vector pointing from the edge between the two planes to the center of the plane
+        const position_type neighbor_plane_unit_inline (sc.get_neighbor_info(pos_structure_id.second, 2).second);
+
+        new_id = neighbor_mapping_[pos_structure_id.second][2].first;
+        neighbor_plane_par = component_y * origin_plane.unit_y();
+        neighbor_plane_inl = (-component_x - origin_length_x)* neighbor_plane_unit_inline;
+    }
+    else if ( (-half_extends[1] < component_y) && ( component_y < half_extends[1]) &&
+              (half_extends[0] < component_x) )
+    {
+        // we are at the 'right' of the plane (side nr. 3)
+        // this is a unit vector pointing from the edge between the two planes to the center of the plane
+        const position_type neighbor_plane_unit_inline (sc.get_neighbor_info(pos_structure_id.second, 3).second);
+
+        new_id = neighbor_mapping_[pos_structure_id.second][3].first;
+        neighbor_plane_par = component_y * origin_plane.unit_y();
+        neighbor_plane_inl = (component_x - origin_length_x)* neighbor_plane_unit_inline;
+    }
+    else
+    {
+        // we are somewhere in the corners and don't know what to do
+        // throw exception?
+    }
+
+    const position_type new_pos ( add(sc.get_structure(new_id)->position(),
+                                      add(neighbor_plane_par, neighbor_plane_inl)));
+    // TODO do normal apply_boundary too?
+    return std::make_pair(new_id, new_pos);
+}
+
+template<>
+inline
+cyclic_transpose
+
+
+
+///// Functions for Cylinders
+
+//// Functions for Boxes
