fc468273c02b64925f14ad8798698411544f7b1b
Thomas Sokolowski <sokolowski@bionet3.amolf.nl>
2012-05-02 19:04:17 +0200

    Forgot to add new files to last commits

---
+++ binding/disk_class.hpp
@@ -0,0 +1,10 @@
+#ifndef BINDING_DISK_CLASS_HPP
+#define BINDING_DISK_CLASS_HPP
+
+namespace binding {
+
+void register_disk_class();
+
+} // namespace binding
+
+#endif /* BINDING_DISK_CLASS_HPP */
